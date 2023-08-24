e240ff12cf76d2876ce566af5112d7142b22a1f3
Chuang Ma <u7703248@anu.edu.au>
2023-08-25 06:41:37 +0800

    I create a class B.

---
+++ src/gittest/B.java
@@ -0,0 +1,16 @@
+package gittest;
+
+import java.util.SplittableRandom;
+
+/**
+ * Description:
+ *
+ * @Author Chuang Ma
+ * @Create 2023/8/25 6:38
+ * Version 1.0
+ */
+public class B {
+    public String toString(){
+        return null;
+    }
+}

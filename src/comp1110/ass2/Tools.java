cbff981a3d4d143d28a25646ff0f4c6c69ad464b
Xing Chen <u7725171@anu.edu.au>
2023-09-28 01:47:43 +1000

    [Xing Chen][Comment]Added some comment for Assam and Tool class

--- src/comp1110/ass2/Tools.java
+++ src/comp1110/ass2/Tools.java
@@ -1,7 +1,7 @@
 package comp1110.ass2;
 
 public class Tools {
-
+    // a simple tool to determine if a string is a valid number
     public  static boolean isNumber(String str) {
         for (char c : str.toCharArray()) {
             if (!Character.isDigit(c)) {
